f731e2022c1bf88fac80f1b06fc5c05f97524977
Lucas Merchant <lbmerchant93@gmail.com>
2020-12-05 18:31:52 -0700

    Complete test for UserReository

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -6,7 +6,16 @@ class UserRepository {
     const foundUser = this.allUsers.findIndex((user) =>  user.id === id);
     return this.allUsers[foundUser];
   }
+  averageUserStepGoals() {
+    const totalStepGoals = this.allUsers.reduce((total, personalGoal) => {
+      total += personalGoal.dailyStepGoal;
+      return total;
+    }, 0);
+    return totalStepGoals / this.allUsers.length;
+  }
 }
+
+
 if (typeof module !== 'undefined') {
   module.exports = UserRepository;
 }
