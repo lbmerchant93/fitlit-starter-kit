3b78528ebee5d741b2277e8f6a4806b2537a36f6
mattdean <matthewallandean@gmail.com>
2020-12-06 10:55:24 -0700

    Display user info on window load

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+//Query Selectors
+let userInfo = document.querySelector(".user-info");
+
+//Event Listeners
+window.addEventListener("load", displayUserInfo);
+
+
+//Global Variables
+let activeUser = new User(sampleData[1]);
+
+console.log(activeUser);
+//Event Handlers and Functions
+function displayUserInfo() {
+  console.log(activeUser);
+  userInfo.innerHTML = `
+  <p class="user-name">Name: ${activeUser.name}</p>
+  <p class="user-address">Address: ${activeUser.address}</p>
+  <p class="user-email">Email: ${activeUser.email}</p>
+  <p class="user-stride-length">Stride Length: ${activeUser.strideLength}</p>
+  <p class="user-daily-step-goal">Daily Step Goal: ${activeUser.dailyStepGoal}</p>
+  `;
+}
+
--- test/User-test.js
+++ test/User-test.js
@@ -10,16 +10,17 @@ describe('User', function() {
 
   beforeEach(function() {
     userData = {"id": 1,
-    "name": "Luisa Hane",
-    "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
-    "email": "Diana.Hayes1@hotmail.com",
-    "strideLength": 4.3,
-    "dailyStepGoal": 10000,
-    "friends": [
-      16,
-      4,
-      8
-    ]};
+      "name": "Luisa Hane",
+      "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+      "email": "Diana.Hayes1@hotmail.com",
+      "strideLength": 4.3,
+      "dailyStepGoal": 10000,
+      "friends": [
+        16,
+        4,
+        8
+      ]
+    };
     user = new User(userData);
   });
 
