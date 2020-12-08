4587d690b1ee55767544fca17379a47d18d9194c
mattdean <matthewallandean@gmail.com>
2020-12-08 14:04:30 -0700

    Add sample data for sleep

---
+++ test/Sleep-test.js
@@ -0,0 +1,12 @@
+const chai = require('chai');
+const expect = chai.expect;
+const sampleData = require('../test/sampleData');
+const Sleep= require('../src/Sleep');
+
+
+describe('Sleep', function() {
+  const sleep1 = new Sleep
+
+
+
+});
